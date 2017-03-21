b80c0852b29be21903ba83645ad5895b21c8364d
btekse <btekse@gmail.com>
2017-03-21 23:58:50 +0100

    14th excercise

---
+++ week-02/day-02/Idea/src/HelloUser.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Balázs on 2017. 03. 21..
+ */
+import java.util.Scanner;
+public class HelloUser {
+  public static void main(String[] args)
+  {
+    // Modify this program to greet user instead of the World!
+    // The program should ask for the name of the user
+    System.out.println("What is your name?");
+    Scanner myScanner = new Scanner(System.in);
+    String name = myScanner.nextLine();
+    System.out.println("Hello, " + name + "!");
+  }
+}
