60932b1420dc473059870afa9ee389132da1e4b0
btekse <btekse@gmail.com>
2017-03-21 13:42:49 +0100

    HelloWorld

---
+++ week-02/day-02/Idea/src/HelloWorld.java
@@ -0,0 +1,23 @@
+/**
+ * Created by Balázs on 2017. 03. 21..
+ */
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Ez a gyerek oylan\n\"okos\"...");
+    System.out.println(1E6);
+    System.out.println(true);
+
+    boolean isMarried = true;
+    String text = "majom";
+    byte b = 127;
+    short peaches = 1;
+    int apples = 123;
+    long pears = 1234;
+    double pi = 3.14;
+    System.out.println(apples);
+    apples = 11;
+    System.out.println(apples);
+
+
+  }
+}
