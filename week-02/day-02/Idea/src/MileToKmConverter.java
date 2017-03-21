ca43709c162877ddbda94c835cce0da376f6c58a
btekse <btekse@gmail.com>
2017-03-22 00:46:14 +0100

    15th exercise

---
+++ week-02/day-02/Idea/src/MileToKmConverter.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Balázs on 2017. 03. 22..
+ */
+import java.util.Scanner;
+public class MileToKmConverter {
+  public static void main(String[] args) {
+    // Write a program that asks for an integer that is a distance in kilometers,
+    // then it converts that value to miles and prints it
+    double kmToMiles = 0.621371;
+
+    System.out.println("Please enter the distance in km: ");
+
+    Scanner sc = new Scanner(System.in);
+    int km = sc.nextInt();
+
+    System.out.println("Your distance in miles: " + km * kmToMiles);
+  }
+}
