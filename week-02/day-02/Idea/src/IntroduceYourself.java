fe1a04a4cac0117803048044a625c272e2455afd
btekse <btekse@gmail.com>
2017-03-21 14:14:17 +0100

    4th exercise

---
+++ week-02/day-02/Idea/src/IntroduceYourself.java
@@ -0,0 +1,23 @@
+/**
+ * Created by Balázs on 2017. 03. 21..
+ */
+public class IntroduceYourself {
+  public static void main(String[] args) {
+    // Write a program that prints a few details to the terminal window about you
+    // It should print each detail to a new line.
+    //  - Your name
+    //  - Your age
+    //  - Your height in meters (as a decimal fraction)
+    //
+    //  Example output:
+    //  John Doe
+    //  31
+    //  1.87
+
+  System.out.println("Balázs Tekse");
+  System.out.println(35);
+  System.out.println(1.75);
+
+
+  }
+}
