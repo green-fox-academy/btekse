508af30a562bc4d932cb7530fe55bd3392be6927
btekse <btekse@gmail.com>
2017-03-21 17:45:53 +0100

    12th exercise

---
+++ week-02/day-02/Idea/src/Swap.java
@@ -0,0 +1,24 @@
+/**
+ * Created by Balázs on 2017. 03. 21..
+ */
+public class Swap {
+  public static void main(String[] args) {
+    int a = 123;
+    int b = 526;
+
+    int temp = a;
+    a = b;
+    b = temp;
+
+    System.out.println("a: " + a);
+    System.out.println("b: " + b);
+
+    // Swap the values of the wariables
+
+
+
+
+    System.out.println(a);
+    System.out.println(b);
+  }
+}
