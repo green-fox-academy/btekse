f7d1089f5cb77916a59af5c0140d08e7bb32c249
btekse <btekse@gmail.com>
2017-03-21 16:37:52 +0100

    7th exercise

---
+++ week-02/day-02/Idea/src/FavoriteNumber.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Balázs on 2017. 03. 21..
+ */
+public class FavoriteNumber {
+  public static void main(String[] args) {
+    int favorite_number = 12;
+    // Store your favorite number in a variable (as a number)
+    // And print it like this: "My favorite number is: 8"
+
+    System.out.println("My favorite number is: " + (favorite_number));
+  }
+}
