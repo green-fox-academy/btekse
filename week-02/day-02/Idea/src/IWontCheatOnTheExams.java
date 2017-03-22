5ec811e911f6fb347bf09072466f3ce012206d87
btekse <btekse@gmail.com>
2017-03-22 11:38:37 +0100

    Add excercise 23

---
+++ week-02/day-02/Idea/src/IWontCheatOnTheExams.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Balázs on 2017. 03. 22..
+ */
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+
+public class IWontCheatOnTheExams {
+  public static void main(String[] args){
+    int a = 0;
+    while (a < 100) {
+      System.out.println("I won't cheat on the exam!");
+      a += 1;
+    }
+  }
+}
+
