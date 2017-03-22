f39b3d0adba69f1d1051f0e1c33c8901a61d6167
btekse <btekse@gmail.com>
2017-03-22 11:46:09 +0100

    Add excersise 24

---
+++ week-02/day-02/Idea/src/PrintEven.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Balázs on 2017. 03. 22..
+ */
+// Create a program that prints all the even numbers between 0 and 500
+public class PrintEven {
+  public static void main(String[] args){
+    for (int i = 0; i < 501; i += 2) {
+      System.out.println(i);
+    }
+  }
+}
+
