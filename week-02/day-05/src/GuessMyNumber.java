4b1c41e19859bdf38d9e34b2e65151aec1cd854e
btekse <btekse@gmail.com>
2017-03-24 00:44:16 +0100

    added manual range, return from for loop

--- week-02/day-05/src/GuessMyNumber.java
+++ week-02/day-05/src/GuessMyNumber.java
@@ -8,19 +8,24 @@ import java.util.Random;
 public class GuessMyNumber {
   public static void main(String[] args) {
 
+    Scanner myScanner = new Scanner(System.in);
+    System.out.println("Enter the range:");
+    int rng = myScanner.nextInt();
+
     Random rand = new Random();
-    int rn = rand.nextInt(100) + 1;
+    int rn = rand.nextInt((rng) + 1);
     System.out.println(rn); // to be removed later
 
     for (int l = 5; l > 0; l--) {
       System.out.println("Count is: " + l); // to be removed later
 
-      Scanner myScanner = new Scanner(System.in);
-      System.out.println("I've the number between 1-100. You have " + l + " lives. Enter number: ");
+      System.out.println("I've got a number between 1-100. You have " + l + " lives. Enter number: ");
       int n1 = myScanner.nextInt();
 
-      if (n1 == rn)
+      if (n1 == rn) {
         System.out.println("Congratulations. You won!");
+        return;
+      }
       else if (n1 > rn)
         System.out.println("Too high.");
       else
