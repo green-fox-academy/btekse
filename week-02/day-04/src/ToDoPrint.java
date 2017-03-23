ce260942dd3a4a3507158ef544d7b1bc8ab12927
btekse <btekse@gmail.com>
2017-03-23 16:11:21 +0100

    Add exercise #4

---
+++ week-02/day-04/src/ToDoPrint.java
@@ -0,0 +1,22 @@
+/**
+ * Created by Balázs on 2017. 03. 23..
+ */
+public class ToDoPrint {
+  public static void main(String[] args){
+    String todoText = " - Buy milk\n";
+    String todoText2 = "My todo:\n" + (todoText) + " - Download games\n\t - Diablo";
+    // #4
+    // Add "My todo:" to the beginning of the todoText
+    // Add " - Download games" to the end of the todoText
+    // Add " - Diablo" to the end of the todoText but with indention
+
+    // Expected output:
+
+    // My todo:
+    //  - Buy milk
+    //  - Download games
+    //      - Diablo
+
+    System.out.println(todoText2);
+  }
+}
