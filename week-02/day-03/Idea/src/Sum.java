5ffb50d2a9553376156fc8a41d0b03b3c834ca4f
btekse <btekse@gmail.com>
2017-03-23 09:42:49 +0100

    Add exercise #4_v2

--- week-02/day-03/Idea/src/Sum.java
+++ week-02/day-03/Idea/src/Sum.java
@@ -12,6 +12,7 @@ import java.util.Scanner;
 public class Sum {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter number: ");
     int x = scanner.nextInt();
     sum(x);
   }
