9bb6b967a6d394eb46cd094a591970cf911675fb
btekse <btekse@gmail.com>
2017-03-24 11:09:00 +0100

    added the 'add lives' option

--- week-02/day-05/src/GuessMyNumber.java
+++ week-02/day-05/src/GuessMyNumber.java
@@ -9,28 +9,31 @@ public class GuessMyNumber {
   public static void main(String[] args) {
 
     Scanner myScanner = new Scanner(System.in);
-    System.out.println("Enter the range:");
+    System.out.println("Enter the maximum range:");
     int rng = myScanner.nextInt();
+
+    System.out.println("Enter the number of lives:");
+    int ln = myScanner.nextInt();
 
     Random rand = new Random();
     int rn = rand.nextInt((rng) + 1);
     System.out.println(rn); // to be removed later
 
-    for (int l = 5; l > 0; l--) {
-      System.out.println("Count is: " + l); // to be removed later
+    for (int l = ln; l > 0; l--) {
+      //System.out.println("Count is: " + l); // to be removed later
 
-      System.out.println("I've got a number between 1-100. You have " + l + " lives. Enter number: ");
+      System.out.println("I've got a number between 1-" + rng + ". You have " + l + " lives. Enter number: ");
       int n1 = myScanner.nextInt();
 
       if (n1 == rn) {
-        System.out.println("Congratulations. You won!");
+        System.out.println("Congratulations. My number was: " + rn + ". You won!");
         return;
-      }
-      else if (n1 > rn)
+      } else if (n1 > rn)
         System.out.println("Too high.");
       else
         System.out.println("Too low.");
+      }
+      System.out.println("Game over. I won.");
 
-    }
   }
 }
