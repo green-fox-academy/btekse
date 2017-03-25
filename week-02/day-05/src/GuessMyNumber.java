14c576e5bffcef4a72be5e434e25b29480fe0eb1
btekse <btekse@gmail.com>
2017-03-25 19:21:07 +0100

    while loop

--- week-02/day-05/src/GuessMyNumber.java
+++ week-02/day-05/src/GuessMyNumber.java
@@ -2,6 +2,10 @@
  * Created by Balázs on 2017. 03. 23..
  */
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+import javax.sound.midi.Soundbank;
 import java.util.Scanner;
 import java.util.Random;
 
@@ -9,33 +13,57 @@ public class GuessMyNumber {
   public static void main(String[] args) {
 
     Scanner myScanner = new Scanner(System.in);
-    System.out.println("Enter the maximum range:");
-    int rng = myScanner.nextInt();
 
-    System.out.println("Enter the number of lives:");
-    int ln = myScanner.nextInt();
+  boolean game = true;
+  while(game)
+    { // addig fut, ameddig ez true
 
-    Random rand = new Random();
-    int rn = rand.nextInt(rng + 1);
-    // System.out.println(rn); // to be removed later
 
-    for (int l = ln; l > 0; l--) {
-      //System.out.println("Count is: " + l); // to be removed later
+      System.out.println("Enter the maximum range:");
+      int rng = myScanner.nextInt();
 
-      System.out.println("I've got a number between 1-" + rng + ". " +
-        "You have " + l + " lives. Enter number: ");
-      int n1 = myScanner.nextInt();
+      System.out.println("Enter the number of lives:");
+      int ln = myScanner.nextInt();
 
-      if (n1 == rn) {
-        System.out.println("Congratulations. My number was: " + rn + "." +
-          " You won!");
-        return;
-      } else if (n1 > rn)
-        System.out.println("Too high.");
-      else
-        System.out.println("Too low.");
+      Random rand = new Random();
+      int rn = rand.nextInt(rng) + 1;
+      // System.out.println(rn); // to be removed later
+
+      boolean foundNumber = false;
+      for (int l = ln; l > 0 && !foundNumber; l--) {
+        //System.out.println("Count is: " + l); // to be removed later
+
+        System.out.println("I've got a number between 1-" + rng + ". " +
+          "You have " + l + " lives. Enter number: ");
+        int n1 = myScanner.nextInt();
+
+        if (n1 == rn) {
+          System.out.println("Congratulations. My number was: " + rn + "." +
+            " You won!");
+          foundNumber = true;
+        } else if (n1 > rn)
+          System.out.println("Too high.");
+        else
+          System.out.println("Too low.");
+      }
+
+
+
+      boolean iscorrectanswer = false;
+      while (!iscorrectanswer) {
+        System.out.println("New game (y/n)?");
+        String newgame = myScanner.next();
+        if ("y".equals(newgame)) {
+          iscorrectanswer = true;
+        } else if ("n".equals(newgame)) {
+          game = false;
+          iscorrectanswer = true;
+          }
+          else
+          System.out.println("Bad answer: "+newgame+" Try another.");
+      }
+
     }
-    System.out.println("Game over. My number was: " + rn + ". I won.");
-
+    System.out.println("Thanks for playing");
   }
 }
