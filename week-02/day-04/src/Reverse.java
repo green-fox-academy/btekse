7c96eeed944f94974ec0c4e4eb9c982d71074c71
btekse <btekse@gmail.com>
2017-03-27 10:34:31 +0200

    modded 0327

--- week-02/day-04/src/Reverse.java
+++ week-02/day-04/src/Reverse.java
@@ -1,13 +1,11 @@
-/**
+﻿/**
  * Created by Balázs on 2017. 03. 23..
  */
 public class Reverse {
   public static void main(String[] args) {
     String reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
 
-    // Create a function that can reverse a String, which is passed as the parameter
-    // Use it on this reversed string to check it!
-    // Try to solve this using charAt() first, and optionally anything else after.
+
     StringBuilder input = new StringBuilder();
     input.append(reversed);
     input = input.reverse();
@@ -18,4 +16,4 @@ public class Reverse {
   }
 }
 
-
+array[][]
---
+++ week-03/day-01/src/running.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Balázs on 2017. 03. 27..
+ */
+public class running {
+  public static void main(String[] args) {
+    int a = 5;
+    int b = 1;
+    try {
+      System.out.println(a / b);
+      System.out.println("It works");
+    } catch (ArithmeticException e) {
+      System.out.println("It doesn't work" + e);
+    }
+
+    System.out.println("Hi");
+  }
+}
+
+}
