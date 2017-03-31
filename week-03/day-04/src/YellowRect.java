b76eb599d2552b7b8e537019f4a3dba9d8a91c63
btekse <btekse@gmail.com>
2017-03-31 09:44:28 +0200

    Add exercise YellowRect final

--- week-03/day-04/src/YellowRect.java
+++ week-03/day-04/src/YellowRect.java
@@ -12,25 +12,27 @@ public class YellowRect {
   //public static int height = 2000;
 
   public static void mainDraw(Graphics g) {
-    rectPosition(g, 0, 0, 800, 800);
+    rectPosition(g, 0, 0, 900);
   }
 
-  public static void rectPosition(Graphics g, int x, int y, int width, int heigth) {
-    if (width < 3 || heigth < 3) {
+  public static void rectPosition(Graphics g, int x, int y, int size) {
+    g.setColor(Color.BLACK);
+    g.drawRect(x, y, size, size);
+
+    if (size < 6) {
       return;
     } else {
-      g.setColor(Color.BLACK);
-      g.drawRect(x, y, width, heigth);
-      rectPosition(g, x + (width / 3), y, width / 3, heigth / 3);
-      rectPosition(g, x, y + (heigth/3), width/3, heigth/3);
-      rectPosition(g, x + (2*(width/3)+1), y + (heigth/3), width/3, heigth/3);
-      rectPosition(g, x + (width / 3), y + (2*(heigth/3)+1), width/3, heigth/3);
+
+      rectPosition(g, x + (size / 3), y, size / 3); //up
+      rectPosition(g, x, y + (size/3), size/3); //left
+      rectPosition(g, x + (2*(size/3)), y + (size/3), size/3); //right
+      rectPosition(g, x + (size / 3), y + (2*(size/3)), size/3); //down
     }
   }
 
   public static void main(String[] args) {
     JFrame jFrame = new JFrame("Drawing");
-    jFrame.setSize(new Dimension(800, 800));
+    jFrame.setSize(new Dimension(900, 900));
     jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
     jFrame.add(new ImagePanel());
     jFrame.setLocationRelativeTo(null);
