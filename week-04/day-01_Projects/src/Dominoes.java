981d8712f0a0752a3d7aea502e0d5b1a9a70212e
btekse <btekse@gmail.com>
2017-04-03 13:39:48 +0200

    Add exercise Domino

--- week-04/day-01_Projects/src/Dominoes.java
+++ week-04/day-01_Projects/src/Dominoes.java
@@ -11,17 +11,34 @@ public class Dominoes {
     // Order them into one snake where the adjacent dominoes have the same numbers on their adjacent sides
     // eg: [2, 4], [4, 3], [3, 5] ...
 
+    /*Domino buffer = new Domino(0, 0);
+
+    for (int i = 0; i < dominoes.size(); i++) {
+      for (int j = i; j < dominoes.size(); j++) {
+        if (dominoes.get(i).getValues()[1] == dominoes.get(j).getValues()[0]) {
+          buffer = dominoes.get(i + 1);
+          dominoes.set(i + 1, dominoes.get(j));
+          dominoes.set(j, buffer);
+        }
+      }
+    }*/
+
+
+
     System.out.println(dominoes);
   }
 
   static List<Domino> initializeDominoes() {
     List<Domino> dominoes = new ArrayList<>();
     dominoes.add(new Domino(5, 2));
-    dominoes.add(new Domino(4, 6));
-    dominoes.add(new Domino(1, 5));
-    dominoes.add(new Domino(6, 7));
     dominoes.add(new Domino(2, 4));
+    dominoes.add(new Domino(4, 6));
+    dominoes.add(new Domino(6, 7));
     dominoes.add(new Domino(7, 1));
+    dominoes.add(new Domino(1, 5));
+
+
+
     return dominoes;
   }
 }
