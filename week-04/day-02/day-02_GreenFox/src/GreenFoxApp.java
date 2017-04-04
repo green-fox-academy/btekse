f8af5323850093efce25eea872ce62e69e4707d4
btekse <btekse@gmail.com>
2017-04-04 11:42:29 +0200

    Add exercise GreenFoxApp basic datas

---
+++ week-04/day-02/day-02_GreenFox/src/GreenFoxApp.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Balázs on 2017. 04. 04..
+ */
+public class GreenFoxApp {
+  public static void main(String[] args) {
+
+    Person myPerson = new Person();
+    myPerson.introduce();
+  }
+}
