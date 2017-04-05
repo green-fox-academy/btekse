3b30d3abf5ad414dad655575c2a73c2b77777696
btekse <btekse@gmail.com>
2017-04-05 14:08:21 +0200

    Add exercise Sum

---
+++ week-04/day-03/day-03_Sum/Test/SumTest.java
@@ -0,0 +1,32 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class SumTest {
+
+  @Test
+  public void testSumClass() throws Exception {
+  List<Integer> numbers = new ArrayList<Integer>(Arrays.asList());
+    Sum newList = new Sum();
+    assertEquals(0, newList.sum(numbers));
+  }
+
+  @Test
+  public void testSumNumbers() throws Exception {
+    List<Integer> numbers = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5));
+    Sum newList = new Sum();
+    assertEquals(15, newList.sum(numbers));
+  }
+
+  @Test
+  public void testSumOneElement() throws Exception {
+    List<Integer> numbers = new ArrayList<Integer>(Arrays.asList(1));
+    Sum newList = new Sum();
+    assertEquals(1, newList.sum(numbers));
+  }
+
+}
---
+++ week-04/day-03/day-03_Sum/src/Sum.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+public class Sum {
+
+  public Sum(){
+  }
+
+  public int sum(List<Integer> numbers){
+    int result = 0;
+
+    for (int i : numbers) {
+      result += i;
+    }
+    if (result == 0){
+      return 0;
+    }
+    return result;
+  }
+}
