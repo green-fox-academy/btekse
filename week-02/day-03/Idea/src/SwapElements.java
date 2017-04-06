23eebc3cca7141184e9df218b23b128cede36474
btekse <btekse@gmail.com>
2017-04-06 02:01:56 +0200

    Add exercise SwapElements

---
+++ week-02/day-03/Idea/src/SwapElements.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+/**
+ * Created by Balázs on 2017. 04. 06..
+ */
+
+// - Create an array variable named `abc`
+//   with the following content: `["first", "second", "third"]`
+// - Swap the first and the third element of `abc`
+
+public class SwapElements {
+  public static void main (String[] args) {
+
+    String[] abc = {"first", "second", "third"};
+
+    String temp;
+    temp = abc[0];
+    abc[0] = abc[2];
+    abc[2] = temp;
+
+    System.out.println(Arrays.toString(abc));
+  }
+}
