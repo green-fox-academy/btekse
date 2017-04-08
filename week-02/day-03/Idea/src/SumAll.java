521cca7ff98f7664acb803aa9d755b2f8e0b9981
btekse <btekse@gmail.com>
2017-04-08 13:24:23 +0200

    add(app) SumAll

---
+++ week-02/day-03/Idea/src/SumAll.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Balázs on 2017. 04. 08..
+ */
+
+// - Create an array variable named `ai`
+//   with the following content: `[3, 4, 5, 6, 7]`
+// - Print the sum of the elements in `ai`
+
+public class SumAll {
+  public static void main (String[] args) {
+    int[] ai = {3, 4, 5, 6, } ;
+    for (int i = 0; i < ai.length; i++) {
+
+    }
+  }
+}
