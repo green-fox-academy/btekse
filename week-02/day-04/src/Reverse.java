66177673cdfadc95b0d3f63afe11359b7801a72d
btekse <btekse@gmail.com>
2017-04-09 19:55:40 +0200

    mod(app) Reverse v2

--- week-02/day-04/src/Reverse.java
+++ week-02/day-04/src/Reverse.java
@@ -1,19 +1,11 @@
-﻿/**
- * Created by Balázs on 2017. 03. 23..
- */
 public class Reverse {
-  public static void main(String[] args) {
+  public static void main(String[] args){
     String reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
 
-
-    StringBuilder input = new StringBuilder();
-    input.append(reversed);
-    input = input.reverse();
-
-    for (int i = 0; i < input.length(); i++)
-
-      System.out.print(input.charAt(i));
+    // Create a function that can reverse a String, which is passed as the parameter
+    // Use it on this reversed string to check it!
+    // Try to solve this using charAt() first, and optionally anything else after.
+    StringBuilder back = new StringBuilder(reversed).reverse();
+    System.out.println(back);
   }
 }
-
-array[][]
