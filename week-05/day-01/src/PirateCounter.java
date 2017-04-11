d398c28928404eadad39607fbc50557e93dd16e9
btekse <btekse@gmail.com>
2017-04-11 18:38:52 +0200

    add(app) : Pirate final

--- week-05/day-01/src/PirateCounter.java
+++ week-05/day-01/src/PirateCounter.java
@@ -2,7 +2,8 @@ import java.util.*;
 
 public class PirateCounter{
 
-  class Pirate {
+  static class Pirate {
+
     String name;
     boolean hasWoodenLeg;
     int gold;
@@ -12,6 +13,18 @@ public class PirateCounter{
       this.hasWoodenLeg = hasWoodenLeg;
       this.gold = gold;
     }
+
+    public String getName() { //getter Alt + Insert
+      return name;
+    }
+
+    public boolean isHasWoodenLeg() { //getter Alt + Insert
+      return hasWoodenLeg;
+    }
+
+    public int getGold() { //getter Alt + Insert
+      return gold;
+    }
   }
 
   public static void main(String... args){
@@ -19,19 +32,27 @@ public class PirateCounter{
 
     // Given this list...
 
-    pirates.add(new Pirate("Olaf", false, 12);
-    pirates.add(new Pirate("Uwe", true, 9);
-    pirates.add(new Pirate("Jack", true, 16);
-    pirates.add(new Pirate("Morgan", false, 17);
-    pirates.add(new Pirate("Hook", true, 20);
+    pirates.add(new Pirate("Olaf", false, 12));
+    pirates.add(new Pirate("Uwe", true, 9));
+    pirates.add(new Pirate("Jack", true, 16));
+    pirates.add(new Pirate("Morgan", false, 17));
+    pirates.add(new Pirate("Hook", true, 20));
+    System.out.println(returnName(pirates));
+    }
 
+  private static List<String> returnName(List<Pirate> listOfPirates) { //method - 1. amit visszaad 2. amit bekér
+    List<String> piratesName = new ArrayList<>(); //deklarálás
+    for (int i = 0; i < listOfPirates.size(); i++) {
+      if (listOfPirates.get(i).hasWoodenLeg && listOfPirates.get(i).getGold() > 15) {
+        piratesName.add(listOfPirates.get(i).getName());
+
+      }
     }
-    {
-      System.out.println("OK");
-    }
-    // Write a function that takes any list that contains pirates as in the example,
+  return piratesName;
+  }
+
+  // Write a function that takes any list that contains pirates as in the example,
     // And returns a list of names containing the pirates that
     // - have wooden leg and
     // - have more than 15 gold
-  }
 }
