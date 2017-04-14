46b3fa1b25afeeb7f4b0f2c93adcb6bb42184643
btekse <btekse@gmail.com>
2017-04-14 16:03:09 +0200

    add(app) : Cuboid revision

--- week-02/day-02/Idea/src/Cuboid.java
+++ week-02/day-02/Idea/src/Cuboid.java
@@ -1,7 +1,3 @@
-/**
- * Created by Balázs on 2017. 03. 21..
- */
-
 // Write a program that stores 3 sides of a cuboid as variables (doubles)
 // The program should write the surface area and volume of the cuboid like:
 //
@@ -11,9 +7,11 @@
 public class Cuboid {
   public static void main(String[] args) {
 
-    double a = 20;
+    double a = 2;
+    double b = 3;
+    double c = 4;
 
-    System.out.println("Surface Area: " + (a * a * 6));
-    System.out.println("Volume: " + (a * a * a));
+    System.out.println("Surface Area: " + ((a * b) + (a * c) + (b * c)) * 2);
+    System.out.println("Volume: " + (a * b * c));
   }
 }
