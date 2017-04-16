ea36aadba916c51bab9397da2c14f84e9e21945e
btekse <btekse@gmail.com>
2017-04-16 19:59:23 +0200

    add(app): AppendA2 revision

--- week-02/day-03/Idea/src/AppendA2.java
+++ week-02/day-03/Idea/src/AppendA2.java
@@ -9,7 +9,7 @@
 
 public class AppendA2 {
   public static void main(String[] args) {
-    String[] nimals = {"kuty", "macs", "cic"};
+    String[] nimals = {"kuty", "macsk", "cic"};
 
     for (int i = 0; i < nimals.length; i++) {
       nimals[i] += "a";
