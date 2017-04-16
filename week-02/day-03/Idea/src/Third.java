e92e33c7ab772cc65f3359e3930a5ee4c8aee8c5
btekse <btekse@gmail.com>
2017-04-16 19:24:33 +0200

    add(app) : Third revision

--- week-02/day-03/Idea/src/Third.java
+++ week-02/day-03/Idea/src/Third.java
@@ -11,7 +11,7 @@ public class Third {
     int[] q = {4, 5, 6, 7};
 
     // Print the second element of the array
-    System.out.println("The third element is " + q[3]);
+    System.out.println("The third element is " + q[2]);
 
     // Print all the array elements
     for (int i = 0; i < q.length; i++) {
