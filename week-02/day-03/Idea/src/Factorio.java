718f1c71411ee7aca7d142c38229e753b2432a2a
btekse <btekse@gmail.com>
2017-04-16 18:34:24 +0200

    add(app) : Factorio revision

--- week-02/day-03/Idea/src/Factorio.java
+++ week-02/day-03/Idea/src/Factorio.java
@@ -1,20 +1,18 @@
 import java.util.Scanner;
 
-/**
- * Created by Balázs on 2017. 03. 23..
- */
 //  #5
 //  Create the usual class wrapper
 //  and main method on your own.
 
 // - Create a function called `factorio`
 //   that returns it's input's factorial
+
 public class Factorio {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    System.out.println("Enter number: ");
-    int x = scanner.nextInt();
-    System.out.println(sum(x));
+    System.out.println("Enter your number to factorial: ");
+    int numToFactorial = scanner.nextInt();
+    System.out.println("The factorial of " + numToFactorial + " is: " + sum(numToFactorial));
   }
 
   public static int sum(int input) {
@@ -22,7 +20,6 @@ public class Factorio {
     int summary = 1;
     for (int i = 1; i <= input; i++) {
       summary *= i;
-      //summary 7 summary * i
     }
     return summary;
   }
