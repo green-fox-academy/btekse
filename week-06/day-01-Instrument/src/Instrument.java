9e6de8af37c1f9070a82b7fb9e3619c8fd4d0894
btekse <btekse@gmail.com>
2017-04-18 22:28:34 +0200

    add(app) : files

--- week-06/day-01-Instrument/src/Instrument.java
+++ week-06/day-01-Instrument/src/Instrument.java
@@ -1,13 +1,13 @@
 package main.java.music;
 
 public abstract class Instrument {
-  protected String name;
-  protected String sound;
+  String name;
+  String sound;
 
   public Instrument(String name, String sound) {
     this.name = name;
     this.sound = sound;
   }
 
-  abstract void play(); //play method
+  public abstract void play(); //play method
 }
--- week-06/day-01-Instrument/src/StringedInstrument.java
+++ week-06/day-01-Instrument/src/StringedInstrument.java
@@ -1,6 +1,11 @@
 package main.java.music;
 
 public abstract class StringedInstrument extends main.java.music.Instrument {
-  protected int numberOfStrings;
+  int numberOfStrings;
+  final String formatForPlay ="%s, a %d-stringed instrument that %s\n";
 
+  public StringedInstrument(String name, String sound, int numberOfStrings) {
+    super(name, sound);
+    this.numberOfStrings = numberOfStrings;
+  }
 }
