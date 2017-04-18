a9cde81c4c7213229eba0826f6b542ea25a28dc0
btekse <btekse@gmail.com>
2017-04-18 21:41:38 +0200

    add(app) : Instrument modded

--- week-06/day-01-Instrument/src/Instrument.java
+++ week-06/day-01-Instrument/src/Instrument.java
@@ -2,5 +2,12 @@ package main.java.music;
 
 public abstract class Instrument {
   protected String name;
+  protected String sound;
+
+  public Instrument(String name, String sound) {
+    this.name = name;
+    this.sound = sound;
+  }
+
   abstract void play(); //play method
 }
---
+++ week-06/day-01-Instrument/src/StringedInstrument.java
@@ -0,0 +1,6 @@
+package main.java.music;
+
+public abstract class StringedInstrument extends main.java.music.Instrument {
+  protected int numberOfStrings;
+
+}
