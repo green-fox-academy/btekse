84b5b49c4c0d45579cd4bd07bcf11b99e26c761b
btekse <btekse@gmail.com>
2017-04-26 10:48:39 +0200

    add(app) : CAB random generate, scanner

---
+++ week-04/day-03/day-03_CowsAndBulls/src/CABMain.java
@@ -0,0 +1,6 @@
+public class CABMain {
+
+  static void play() {
+
+  }
+}
--- week-04/day-03/day-03_CowsAndBulls/src/CowsAndBulls.java
+++ week-04/day-03/day-03_CowsAndBulls/src/CowsAndBulls.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class CowsAndBulls {
-  public static void main(String[] args) {
-    
+  private int level;
+  List<Integer> randomNuber;
+
+  public CowsAndBulls(int level) {
+    this.level = level;
+    this.randomNuber = new ArrayList<>();
+    for (int i = 0; i < level; i++) {
+      int randomN = (int) (Math.random() * 10);
+      randomNuber.add(randomN);
+    }
+  }
+
+  public CowsAndBulls() {
+    this.randomNuber = new ArrayList<>();
+    Scanner scanner = new Scanner(System.in);
+    int userInput = scanner.nextInt();
   }
 }
