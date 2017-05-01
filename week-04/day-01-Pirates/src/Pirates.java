a6ace708d524eff39d73f8257f741442ee43faf8
btekse <btekse@gmail.com>
2017-05-01 10:24:33 +0200

    add(app) Pirates files

---
+++ week-04/day-01-Pirates/src/Pirates.java
@@ -0,0 +1,16 @@
+public class Pirates {
+  private String pirateName;
+  private int rumLevel;
+  private boolean isAlive;
+  private boolean hasParrot;
+
+  public Pirates (String pirateName) {
+    this.pirateName = pirateName;
+    this.rumLevel = rumLevel;
+    this.isAlive = true;
+    this.hasParrot = true;
+  }
+
+
+
+}
