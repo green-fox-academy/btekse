dafb6e67abf056339c2741647f16644c66335a07
btekse <btekse@gmail.com>
2017-05-02 14:37:17 +0200

    add(app): Hello World REST App fixed

--- week-07/day-02/springstart2/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-07/day-02/springstart2/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -1,11 +1,13 @@
-package controllers;
+package com.greenfoxacademy.springstart.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRESTController {
-  @RequestMapping(value = "/greeting")
+  @RequestMapping
+  @ResponseBody
   public Greeting greeting() {
     Greeting greeting = new Greeting(1, "Hello World");
     return greeting;
