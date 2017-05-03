e7ea6cb50cff44b495acda342080741021640012
btekse <btekse@gmail.com>
2017-05-03 16:54:53 +0200

    add(app): exercise5

---
+++ week-07/day-03/src/main/java/com/greenfoxacademy/controllers/exercise5.java
@@ -0,0 +1,29 @@
+package com.greenfoxacademy.controllers;
+
+import com.greenfoxacademy.BankAccount;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class exercise5 {
+
+  @RequestMapping("/exercise5")
+  public String getFifthEndPoint(Model model) {
+    List<BankAccount> bankAccountList = new ArrayList<>();
+    bankAccountList.add(new BankAccount("Simba", 100, "lion"));
+    bankAccountList.add(new BankAccount("Nala", 200, "lion"));
+    bankAccountList.add(new BankAccount("Pumba", 300, "warthog"));
+    bankAccountList.add(new BankAccount("Timon", 100, "meerkat"));
+    bankAccountList.add(new BankAccount("Scar", 100, "lion"));
+
+    model.addAttribute("bankAccountList", bankAccountList);
+    return "exercise5";
+  }
+}
