acff46da2f3953d096737c25b6bc95375e3a0dad
btekse <btekse@gmail.com>
2017-05-04 12:00:16 +0200

    add(app):helloworldbean basic files and package

---
+++ week-07/day-04/src/main/java/com/greenfoxacademy/beanproject/BeanprojectApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.beanproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanprojectApplication.class, args);
+	}
+}
