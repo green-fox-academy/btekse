8a9a5a0eabf40e4bd416eb0dbe0640e02bd0988f
btekse <btekse@gmail.com>
2017-05-09 13:39:07 +0200

    add(app): ToDo class

--- week-08/day-02/src/main/java/com/greenfoxacademy/ToDo.java
+++ week-08/day-02/src/main/java/com/greenfoxacademy/ToDo.java
@@ -1,5 +1,20 @@
 package com.greenfoxacademy;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ToDo {
-  
+  int id;
+  String title;
+  boolean isUrgent;
+  boolean isDone;
+
+  public ToDo (int id, String title, boolean isUrgent, boolean isDone) {
+    this.id = id;
+    this.title = title;
+    this.isUrgent = false;
+    this.isDone = false;
+  }
 }
---
+++ week-08/day-02/src/main/java/com/greenfoxacademy/controller/TodoController.java
@@ -0,0 +1,7 @@
+package com.greenfoxacademy.controller;
+
+/**
+ * Created by Balázs on 2017. 05. 09..
+ */
+public class TodoController {
+}
